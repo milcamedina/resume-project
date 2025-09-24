907443b03795f0563d8ee0f97be69fdc5a88ee62
milcamedina <milcamedina@oru.edu>
2025-09-23 20:41:46 -0500

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,14 @@
+//Define Variables
+let myName = "Milca";
+let myEmail = "milcamedina@oru.edu";
+let title ="Logistics";
+let experience = "Customer Service";
+let education = "Oral Roberts University";
+//let skills = "spmething"; //TODO
+
+//Connect Variables to the HTML (ligaments)
+document .getElementById("my-name").textContent = myName;
+document .getElementById("my-email").textContent = myEmail;
+document .getElementById("title").textContent = title;
+document .getElementById("experience").textContent = experience;
+document .getElementById("education").textContent = education;
